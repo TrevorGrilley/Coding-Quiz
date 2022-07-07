8af0dc27362ba582e0a22f2798f0dbf6603438ad
Trevor <trevorgrilley@yahoo.com>
2022-07-07 15:42:45 -0400

    added variables in js by html ids

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//DOM Element Variables
+
+var questionEl = document.getElementById("quiz-questions");
+var choicesEl = document.getElementById("choices");
+var responseEl = document.getElementById("response");
+var startBtn = document.getElementById("start-quiz");
+var submitBtn = document.getElementById("submit");
+var timeEl = document.getElementById("time");
+var initialsEl = document.getElementById("initials");
+var currentQuestion;
+var currentQuestionIndex = 0;
