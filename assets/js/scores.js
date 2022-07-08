f5af6776a9b48c581254e633a8b4aaacf9d567be
Trevor <trevorgrilley@yahoo.com>
2022-07-07 21:52:16 -0400

    created list for highscores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,13 @@
 //The variables specific to js on the scores.html page
-var highscores = document.querySelector("#highscores");
+const highScoresList = document.getElementById('highScoresList');
+const highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+highScoresList.innerHTML = highscores
+.map (score=> {
+    return ('<li class="high-score">${initials}-${time}</li>');
+})
+.join("");
+
 var reset = document.querySelector("#reset");
 var back = document.querySelector("#back");
 // Function call and storage of the resetting of scores
@@ -7,29 +15,7 @@ reset.addEventListener("click", function(){
     localStorage.clear();
     location.reload();
 });
-function saveHighscores() {
-    //Initials are saved when input by player
-    var initials = initialsEl.value;
-  
-    //Ensure the box contains a value
-    if (initials !== "") {
-        //Request the saved scores from the local storage
-        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
-        //When new score happens, input initials
-        var newScore = {
-            score: time,
-            initials: initials
-        };
-  
-        //Save to local storage
-        highscores.push(newScore);
-        window.localStorage.setItem("highscores", JSON.stringify(highscores));
-  
-        ///When clicked, brought to page of highscores
-        window.location.href = "highscores.html"
-    }
-}
+
 // Return to the original page to begin quiz again
 back.addEventListener("click", function () {
     window.location.replace("./index.html");
